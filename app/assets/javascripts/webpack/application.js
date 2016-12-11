ce3fb51c739fa7013e654b1745e422b0938b727d
nonpro <nonprofessional0111@gmail.com>
2016-12-11 15:41:35 +0900

    react hello, world

--- app/assets/javascripts/webpack/application.js
+++ app/assets/javascripts/webpack/application.js
@@ -52,7 +52,15 @@
 
 	var _reactDom = __webpack_require__(32);
 
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	_reactDom2.default.render(_react2.default.createElement(
+	  'h1',
+	  null,
+	  'Hello, world!'
+	), document.getElementById('container'));
 
 /***/ },
 /* 1 */
